5b4c3b630762bbf151283a3c7c41716957fbbb1c
Samu22bd <samesg04@gmail.com>
2024-10-11 17:44:43 +0700

    -fill relevant information

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,21 +13,21 @@ export const About = () => {
                 <img src={getImageUrl('about/serverIcon.png')} alt="Server image" />
                 <div className={styles.aboutItemText}>
                     <h3>Back End</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p>Have experiences doing back end with php, laravel, C#, and .NET.</p>
                 </div>
             </li>
             <li className={styles.aboutItem}>
                 <img src={getImageUrl('about/cursorIcon.png')} alt="Cursor image" />
                 <div className={styles.aboutItemText}>
                     <h3>Front End</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pers</p>
+                    <p>Have experiences in html, css, and js. Including using frameworks like react.</p>
                 </div>
             </li>
             <li className={styles.aboutItem}>
                 <img src={getImageUrl('about/uiIcon.png')} alt="UI image" />
                 <div className={styles.aboutItemText}>
                     <h3>UI</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Srep</p>
+                    <p>Have experiences in figma.</p>
                 </div>
             </li>
         </ul>
