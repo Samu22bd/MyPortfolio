7bdfeb6e5360bd9630f714c8c17ee29a00f9a290
Samu22bd <samesg04@gmail.com>
2024-10-17 10:32:44 +0700

    -fix more css

--- src/components/Me/Me.jsx
+++ src/components/Me/Me.jsx
@@ -6,12 +6,10 @@ export const Me = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Samuel</h1>
-                <p className={styles.description}>I'm a software Engineer with some experience in java, php, react, laravel</p>
+                <p className={styles.description}>I'm a Software Engineer with experience in java, php, react, laravel, and more.</p>
                 <a className={styles.contactBtn} href="mailto:samesg04@gmail.com">Contact Me</a>
             </div>
             <img className={styles.myImage} src={getImageUrl("me/myImage.png")} alt="My Image" />
-            <div className={styles.topBlur}></div>
-            <div className={styles.botBlur}></div>
         </section>
     )
     
